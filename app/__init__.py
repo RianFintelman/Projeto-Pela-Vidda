4f30a3750cf7d101b29f7dfa16598228b936ebb2
Rian Fintelman <14988027708@COSEAC-Info.local>
2026-09-11 15:46:56 -0300

    estrutura inicial do projeto

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from dotenv import load_dotenv  
+import os 
+
+load_dotenv()  
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+
+    # Registra as rotas (blueprints)
+    from app.routes.main import main_bp
+    app.register_blueprint(main_bp)
+
+    return app  
